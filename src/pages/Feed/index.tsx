4ae8bc65243ca151b6e82c7f769097e9a6e5caba
Zeeshan Bhati <zeeshan@plumhq.com>
2024-12-12 21:45:29 +0530

    File Upload

--- src/pages/Feed/index.tsx
+++ src/pages/Feed/index.tsx
@@ -1,17 +1,33 @@
-
 import { Heading } from '../../components/Heading';
-import ackoLogo from '../../assets/logo.svg'
-import '../../App.css'
+import ackoLogo from '../../assets/logo.svg';
+import '../../App.css';
+import FileUpload from '../../components/FileUpload';
 
 const Feed = () => {
+  const handleUploadSuccess = (files: File): void => {
+    console.log('Upload successful:', files);
+  };
+
+  const handleUploadError = (error: Error): void => {
+    console.error('Upload failed:', error);
+  };
   return (
     <div style={{ padding: '16px' }}>
       <a href="https://acko.com" target="_blank">
         <img src={ackoLogo} className="logo_acko" alt="Logo" />
       </a>
       <Heading>Your Feed!</Heading>
+
+      <div>
+        <FileUpload
+          proposalId="your-proposal-id"
+          userId="your-user-id"
+          onUploadSuccess={handleUploadSuccess}
+          onUploadError={handleUploadError}
+        />
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Feed;
